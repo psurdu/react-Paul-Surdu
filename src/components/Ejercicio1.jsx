81432cdf99a1d1cf3fef2500cecc72378b0b2abd
psurdu <psurdu01@educantabria.es>
2022-02-07 11:33:26 +0100

    Inicial

--- src/components/Ejercicio1.jsx
+++ src/components/Ejercicio1.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
 
 class Ejercicio1 extends React.Component {
   constructor(props) {
@@ -7,6 +8,38 @@ class Ejercicio1 extends React.Component {
 
   render() {
     return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      /*
       <div id="ej1">
         <h2>Ejercicio 1</h2>
         <ul>
@@ -39,7 +72,7 @@ class Ejercicio1 extends React.Component {
             Añade al proyecto las dependencias que consideres necesarias.
           </li>
         </ul>
-      </div>
+      </div>*/
     );
   }
 }
